e9a032a61a7e762789a098c49e838418db47b030
Luli080207 <skketchead@gmail.com>
2025-12-06 14:23:44 -0300

    implementacion de servicio de productos con filtros, CRUD y orden

---
+++ backend_utn/src/services/productService.ts
@@ -0,0 +1,42 @@
+import { ProductModel, IProduct } from "src/model/ProductModel.ts";
+
+type Filters = {
+  category?: string;
+  minPrice?: number;
+  maxPrice?: number;
+  name?: string;
+  limit? number;
+  offset?: number;
+};
+
+export const listProducts = async (filters: Filters) => {
+  const query: any = {};
+  if (filters.category) query.category = filters.category;
+  if (filters.minPrice !== undefined) query.price = { ...(query.price || {}), $gte: filters.minPrice};
+  if (filters.maxPrice !== undefined) query.price = { ...(query.price || {}), $lte: filters.maxPrice};
+  if (filters.name) query.name = {$regex: filters.name, $options: "i"};
+
+  const q = ProductModel.find(query).sort({createdAt: -1});
+  if (filters.limit) q.limit(filters.limit);
+  if (filters.offset) q.skip(filters.offset);
+
+  const results = await q.exec();
+  return results;
+};
+
+export const createProduct = async (payload: Partial<IProduct>) => {
+  const p = new ProductModel(payload);
+  return await p.save();
+};
+
+export const getProductById = async (id: string) => {
+  return await ProductModel.findById(id).exec();
+};
+
+export const updateProduct = async (id: string, payload: Partial<IProduct>) => {return await ProductModel.findByIdAndUpdate(id, payload, {new: true}).exec();
+};
+
+export const deleteProduct = async (id: string) => {
+  return await ProductModel.findByIdAndDelete(id).exec();
+};
+
